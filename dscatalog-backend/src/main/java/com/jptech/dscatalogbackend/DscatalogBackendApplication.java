58e13c149bee73056feaba551031c0cf98664795
joaopedromendesjava <joaopedromendessilva@gmail.com>
2024-04-24 22:11:37 -0300

    Created project dscatalog backend

---
+++ dscatalog-backend/src/main/java/com/jptech/dscatalogbackend/DscatalogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jptech.dscatalogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogBackendApplication.class, args);
+	}
+
+}
---
+++ dscatalog-backend/src/test/java/com/jptech/dscatalogbackend/DscatalogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jptech.dscatalogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
