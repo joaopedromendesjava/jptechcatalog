d97c00d06fabbead380a4470517b16eea7953cc1
joaopedromendesjava <joaopedromendessilva@gmail.com>
2024-05-30 18:30:05 -0300

    token additional information

---
+++ jptechcatalog-backend/src/main/java/com/jptech/dscatalogbackend/components/JwtTokenEnhancer.java
@@ -0,0 +1,42 @@
+package com.jptech.dscatalogbackend.components;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.stereotype.Component;
+
+import com.jptech.dscatalogbackend.entities.User;
+import com.jptech.dscatalogbackend.repositories.UserRepository;
+
+// entra no ciclo de vida e adiciona as informações abaixo
+
+@Component
+public class JwtTokenEnhancer implements TokenEnhancer{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+		
+		User user = userRepository.findByEmail(authentication.getName());
+		
+		Map<String, Object> map = new HashMap<>();
+		
+		map.put("userid", user.getId());
+		map.put("userFirstName", user.getFirstName());
+		map.put("userLastName", user.getLastName());
+		
+		DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) accessToken;
+		token.setAdditionalInformation(map);
+		
+		return accessToken;
+		
+	}
+	
+}
